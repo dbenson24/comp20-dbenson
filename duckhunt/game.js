85f474cabb25cbecaab69beb8682545bbced4221
Derek Benson <derek.benson@tufts.edu>
2015-10-07 00:53:50 +0000

    one flying bird

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -41,17 +41,42 @@
 
   })();
 
-  init = function() {
-    var backgroundImg, canvas, context;
+  window.init = function() {
+    var actorsImg, backgroundImg, canvas, context, drawSprites, sprites;
     canvas = document.getElementById("game_canvas");
     context = canvas.getContext("2d");
-    backgroundImg = new Image(800, 600);
-    backgroundImg.src = "./duckhunt-background.gif";
-    return backgroundImg.onload = function() {
-      var frames, sprite;
+    sprites = [];
+    backgroundImg = new Image();
+    backgroundImg.onload = function() {
+      var frames, sprite, temp;
       frames = [];
       frames.push(new Frame(0, 0, 256, 240));
       sprite = new Sprite(context, backgroundImg, 0, 0, 800, 600, frames);
-      return sprite.draw();
+      temp = [sprite];
+      return sprites = temp.concat(sprites);
     };
+    backgroundImg.src = "./duckhunt-background.gif";
+    actorsImg = new Image();
+    actorsImg.onload = function() {
+      var bird1, bird1Frames;
+      bird1Frames = [];
+      bird1Frames.push(new Frame(0, 113, 40, 40));
+      bird1Frames.push(new Frame(40, 113, 40, 40));
+      bird1Frames.push(new Frame(80, 113, 40, 40));
+      bird1 = new Sprite(context, actorsImg, 250, 150, 80, 80, bird1Frames);
+      setInterval(bird1.changeFrame, 150);
+      return sprites.push(bird1);
+    };
+    actorsImg.src = "./duckhunt_various_sheet.png";
+    drawSprites = function() {
+      var s, _i, _len, _results;
+      context.clearRect(0, 0, 800, 600);
+      _results = [];
+      for (_i = 0, _len = sprites.length; _i < _len; _i++) {
+        s = sprites[_i];
+        _results.push(s.draw());
+      }
+      return _results;
+    };
+    return setInterval(drawSprites, 30);
   };
