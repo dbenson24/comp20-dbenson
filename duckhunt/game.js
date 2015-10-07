cab01dbce30eff00bf7904e1139fe6133b1935ba
Derek Benson <derek.benson@tufts.edu>
2015-10-07 00:09:31 +0000

    background is loading in

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+var init = function() {
+    var background, canvas, context;
+    canvas = document.getElementById("game_canvas");
+    context = canvas.getContext("2d");
+    background = new Image(800, 600);
+    background.src = "./duckhunt-background.gif";
+    background.onload = function() {
+      context.drawImage(background, 0, 0, 800, 600);
+    };
+    console.log(background);
+  };
