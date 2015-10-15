15592743f33f193ce57587bad888095c3b6a3358
Derek Benson <derek.benson@tufts.edu>
2015-10-15 02:28:26 +0000

    Proper posting of the data

--- mapchat/scripts/index.js
+++ mapchat/scripts/index.js
@@ -11,12 +11,25 @@
   };
 
   success = function(pos) {
-    var crd;
+    var crd, http, params, url;
     crd = pos.coords;
     console.log('Your current position is:');
     console.log('Latitude : ' + crd.latitude);
     console.log('Longitude: ' + crd.longitude);
-    return console.log('More or less ' + crd.accuracy + ' meters.');
+    console.log('More or less ' + crd.accuracy + ' meters.');
+    http = new XMLHttpRequest();
+    url = "https://secret-about-box.herokuapp.com/sendLocation";
+    params = "login=EricDapper&lat=" + crd.latitude + "&lng=" + crd.longitude + "&message=Hello%20World";
+    console.log(params);
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.onreadystatechange = function() {
+      if (http.readyState === 4 && http.status === 200) {
+        console.log(http.responseText);
+        return alert(http.responseText);
+      }
+    };
+    return http.send(params);
   };
 
   error = function(error) {
